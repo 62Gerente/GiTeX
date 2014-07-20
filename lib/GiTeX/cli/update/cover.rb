e3acd1c63016ac041adbc4e01eb84026c6edafb3
André Santos <andreccdr@gmail.com>
2014-07-20 15:34:24 +0100

    Update cover

--- lib/GiTeX/cli/update/cover.rb
+++ lib/GiTeX/cli/update/cover.rb
@@ -13,7 +13,43 @@ module GiTeX
     end
 
     def run
-      puts "wwoooww"
+      update_cover
+    end
+
+    private
+
+    def update_cover
+      move_cover
+
+      case @identifier
+      when "university"
+        university_cover 
+      end
+    end
+
+    def move_cover
+      FileUtils.cp(Dir["#{Directory.templates}/covers/#{@identifier}.tex"], "#{@repo_dir}/cover.tex")
+    end
+
+    def university_cover
+      cover = "#{@repo_dir}/cover.tex"
+      
+      institution = options[:institution] ? options[:institution] : ''
+      major = options[:major] ? options[:major] : ''
+      minor = options[:minor] ? options[:minor] : ''
+      supervisor = options[:supervisor] ? options[:supervisor] : ''
+
+      content = File.read(cover)
+
+      content = content.gsub(/## INSTITUTION ##/, institution) || content
+      content = content.gsub(/## MAJOR ##/, major)             || content
+      content = content.gsub(/## MINOR ##/, minor)             || content
+      content = content.gsub(/## DATE ##/, @date)              || content
+      content = content.gsub(/## TITLE ##/, @title)            || content
+      content = content.gsub(/## AUTHOR ##/, @author)          || content
+      content = content.gsub(/## SUPERVISOR ##/, supervisor)   || content
+
+      File.open("#{@repo_dir}/cover.tex", "w") {|file| file.puts content}
     end
   end
 end
